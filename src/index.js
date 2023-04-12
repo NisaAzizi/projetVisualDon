4ce66a493ad36ae12c2b76778b8971b4aa8a521a
Caroline Hache <hachecaroline@hotmail.com>
2023-04-12 15:57:37 +0200

    Correct donnesCombinee (undefined to data)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import * as d3 from "d3";
+import {select, selectAll} from "d3-selection";
+import {scaleSqrt, scaleLinear, scalePow} from "d3-scale";
+import {max, min} from "d3-array";
+import {axisLeft, axisBottom} from "d3-axis";
+import {geoMercator, geoPath} from "d3-geo";
+import {json} from "d3-fetch";
+import {transition} from "d3-transition";
+import {easeLinear} from "d3-ease";
 
 import graphe1 from "../data/graph1_FoudreVsHuman.json"
 import graph3Incendie from "../data/graph3_Nombres_incendies_par_provinces_et_années.json"
@@ -90,7 +98,7 @@ const currentYearText = svg.append("text")
 //https://codepen.io/romanoe/pen/rNZKjvB Pour automatiser l'avancement des dates
 d3.select("body")
     .append("button")
-    .text("Suivant")
+    .text("Next")
     .on("click", () => {
         let currentYear = parseInt(bars.data()[0].data.Annee);
         let currentIndex = graphe1.findIndex(d => d.Annee === currentYear);
@@ -105,7 +113,7 @@ d3.select("body")
                 enter => enter.append("rect")
                     .attr("y",  height/2)
                     .attr("height", yScale.bandwidth())
-                    .attr("fill", d => colorScale(d.key))
+                    .attr("fill", d => couleurScale(d.key))
                     .attr("x", d => xScale(d[0]))
                     .attr("width", 0)
                     .call(enter => enter.transition()
@@ -146,33 +154,147 @@ let nbrIncendies = [],
 
     donnesCombinee = [];
 
-const mergeByProvince = (a1, a2, a3) => {
-  let data = [];
-  a1.map(itm => {
-    let newObject = {
-      "Juridiction": itm.Juridiction,
-      "incendies": a3.find((item) => item.Juridiction === itm.Juridiction).nombre,
-      "coût des dommages": a2.find((item) => item.Juridiction === itm.Juridiction).valeur,
-      "superficie": itm.superficie
-    };
-    data.push(newObject);
-  });
-  return data;
-};
-
-annees.forEach(annee => {
-  valDommages.push({"annee":annee, "data" : graph3Dommages})
-  superficieProvinces.push({"annee":annee, "data" :graph3Superficie})
-  nbrIncendies.push({"annee":annee, "data" : graph3Incendie})
-
-  const valDommagesAnnee = valDommages.filter(d => d.annee == annee).map(d => d.data)[0];
-  const superficieProvincesAnnee = superficieProvinces.filter(d => d.annee == annee).map(d => d.data)[0];
-  const nbreIncendieAnnee = nbrIncendies.filter(d => d.annee == annee).map(d => d.data)[0];
-
-  donnesCombinee.push({"annee": annee, "data": mergeByProvince(valDommagesAnnee, superficieProvincesAnnee, nbreIncendieAnnee)})
-});
+    const mergeByProvince = (a1, a2, a3, annee) => {
+        let data = [];
+        a1.map(itm => {
+          
+          let newObject = {
+            "Juridiction": itm.Juridiction,
+            "Incendie": a3.find((item) => (item.Juridiction === itm.Juridiction) && item)[annee],
+            "Dommage": a2.find((item) => (item.Juridiction === itm.Juridiction) && item)[annee],
+            "Superficie": itm.km
+            
+          };
+          data.push(newObject);
+        });
+        return data;
+      };
+      
+      annees.forEach(annee => {
+        superficieProvinces.push({"annee":annee, "data" :graph3Superficie})
+        valDommages.push({"annee":annee, "data" : graph3Dommages})
+        nbrIncendies.push({"annee":annee, "data" : graph3Incendie})
+      
+        const valDommagesAnnee = valDommages.filter(d => d.annee == annee).map(d => d.data)[0];
+        const superficieProvincesAnnee = superficieProvinces.filter(d => d.annee == annee).map(d => d.data)[0];
+        const nbreIncendieAnnee = nbrIncendies.filter(d => d.annee == annee).map(d => d.data)[0];
+      
+        donnesCombinee.push({"annee": annee, "data": mergeByProvince(superficieProvincesAnnee,valDommagesAnnee, nbreIncendieAnnee, annee)})
+      });
 
 console.log(donnesCombinee);
 //ICI OK
+
+
+// Visualisation statique //
+// Data 2019
+const data2019 = donnesCombinee.filter(d => d.annee == 2019).map(d => d.data)[0]
+
+
+const margin = {top : 10, right: 40, bottom: 20, left: 40}
+
+const figure = select('#vizArea')
+    .append('svg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+const superficieProvincesScale = scaleSqrt()
+    .domain([0,max(data2019.map(d => d.superficie))])
+    .range([2,30]);
+
+const nbrIncendiesScale = scaleLinear()
+    .domain([0,max(data2019.map(d => d.Incendie))])
+    .range([0,width]);
+
+const valDommagesScale = scalePow()
+    .domain([0,max(data2019.map(d => d.Dommage))])
+    .range([height,0])
+        .exponent(3);
+
+figure.append('g')
+    .attr("transform", "translate(0," + height + ")")
+    .call(axisBottom(nbrIncendiesScale));
+
+figure.append('g')
+    .call(axisLeft(valDommagesScale));
+
+figure.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("nbrIncendies");
+
+figure.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("valDommages");
+
+
+// Animation
+// variable to store our intervalID
+let nIntervId;
+
+function animate() {
+    // check if already an interval has been set up
+    if (!nIntervId) {
+        nIntervId = setInterval(play, 100);
+    }
+}
+
+let i = 0;
+function play() {
+    if(donnesCombinee[i].annee == 2021 ) {
+        i = 0;
+    } else {
+        i++;
+    }
+
+    select('#paragraphe').text(donnesCombinee[i].annee)
+    updateChart(donnesCombinee[i].data);
+
+}
+
+function stop() {
+    clearInterval(nIntervId);
+    // release our intervalID from the variable
+    nIntervId = null;
+}
+
+
+function updateChart(data_iteration) {
+    figure.selectAll('circle')
+        .data(data_iteration)
+        .join(enter => enter.append('circle')
+                .attr('cx', d => nbrIncendiesScale(d.Incendie))
+                .attr('cy', d => valDommagesScale(d.Dommage))
+                .attr('r', 0)
+                .transition(transition()
+                    .duration(500)
+                    .ease(easeLinear))
+                .attr('r', d=> superficieProvincesScale(d.Superficie))
+                .attr('fill', 'rgba(0,0,0,.5)')
+            ,
+            update => update.transition(transition().duration(500).ease(easeLinear))
+                        .attr('cx', d => nbrIncendiesScale(d.Incendie))
+                        .attr('cy', d => valDommagesScale(d.Dommage)).attr('r', d=> superficieProvincesScale(d.Superficie)),
+            exit => exit.remove())
+}
+
+
+//document.getElementById("play").addEventListener("click", animate);
+//document.getElementById("stop").addEventListener("click", stop);
+
+
+
+
+
+
 
 
